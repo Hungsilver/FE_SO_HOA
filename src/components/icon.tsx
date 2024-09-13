be16a7bba410e67e06983365a770233aadff9e07
VELT <nguyenminhduc7899@gmail.com>
2024-09-13 15:30:58 +0700

    czxczx

---
+++ src/components/icon.tsx
@@ -0,0 +1,13 @@
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+const Icon = () => {
+    return(
+        <div>
+            <FontAwesomeIcon icon={faHouse} />
+        </div>
+    );
+}
+
+export default Icon;
